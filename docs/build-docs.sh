dde5e7541fcf6fc051b34635251e915ae60bead0
Tapio Vierros <tapiovierros@gmail.com>
2010-03-03 21:55:39 +0200

    More sophisticated build-docs script, also builds html readme.

--- docs/build-docs.sh
+++ docs/build-docs.sh
@@ -2,6 +2,16 @@
 # Script name: build-docs.sh
 # Description: Builds documentation using Natural Docs
 
-NaturalDocs -i .. -o HTML HTML -p nd
+ND="NaturalDocs"
+MD="markdown"
 
+echo "Building API documentation..."
+$ND -h >/dev/null 2>&1 || echo "Cannot find NaturalDocs!"
+$ND -i .. -o HTML HTML -p nd || echo "API documentation building failed."
+
+echo "Building Readme..."
+$MD -v >/dev/null 2>&1 || echo "Cannot find markdown!"
+$MD ../README.markdown > ../Readme.html || echo "Readme building failed."
+
+echo "All done."
 
