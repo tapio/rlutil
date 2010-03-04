09ecf2e51cb03352f2ccd44e14e692420af7dedc
Tapio Vierros <tapiovierros@gmail.com>
2010-03-04 13:05:44 +0200

    Change Readme.html output directory to docs.

--- docs/build-docs.sh
+++ docs/build-docs.sh
@@ -11,7 +11,7 @@ $ND -i .. -o HTML HTML -p nd || echo "API documentation building failed."
 
 echo "Building Readme..."
 $MD -v >/dev/null 2>&1 || echo "Cannot find markdown!"
-$MD ../README.markdown > ../Readme.html || echo "Readme building failed."
+$MD ../README.markdown > Readme.html || echo "Readme building failed."
 
 echo "All done."
 
