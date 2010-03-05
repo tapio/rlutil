499d3cc76c549338f0e75a9ed6a9ffdaf9685c5b
Tapio Vierros <tapiovierros@gmail.com>
2010-03-05 14:22:49 +0200

    Add test for arrow keys.

--- test.cpp
+++ test.cpp
@@ -78,25 +78,47 @@ int main() {
 		std::cout << "You should be able to move the '@' character with WASD keys." << std::endl;
 		std::cout << "Hit Space to continue to the next test." << std::endl;
 		std::cout << "Turn count: " << cnt << std::endl;
-		rlutil::locate(x,y); std::cout << "@" << std::endl; // Output player
+		rlutil::gotoxy(x,y); std::cout << "@" << std::endl; // Output player
 		while (true) {
-			rlutil::locate(1,4); std::cout << "Turn count: " << cnt << std::endl;
+			rlutil::locate(1,4); std::cout << "Turn count: " << cnt;
 			if (kbhit()) {
 				char k = getch(); // Get character
-				rlutil::locate(x,y); std::cout << " " << std::endl; // Erase player
+				rlutil::locate(x,y); std::cout << " "; // Erase player
 				if (k == 'a') --x;
 				else if (k == 'd') ++x;
 				else if (k == 'w') --y;
 				else if (k == 's') ++y;
 				else if (k == ' ') break;
-				rlutil::locate(x,y); std::cout << "@" << std::endl; // Output player
+				rlutil::locate(x,y); std::cout << "@"; // Output player
 			}
 			cnt++;
+			fflush(stdout);
 		}
 		rlutil::showcursor();
 	}
 
-	std::cout << "Test 8: Delay" << std::endl;
+	// Test 8
+	{	int x = 7; int y = 7;
+		rlutil::cls();
+		std::cout << "Test 8: Arrow keys" << std::endl;
+		std::cout << "You should be able to move the '@' character with arrow keys." << std::endl;
+		std::cout << "Hit Escape to continue to the next test." << std::endl;
+		rlutil::gotoxy(x,y); std::cout << "@" << std::endl; // Output player
+		while (true) {
+			if (kbhit()) {
+				int k = rlutil::getkey(); // Get character
+				rlutil::locate(x,y); std::cout << " " << std::endl; // Erase player
+				if (k == rlutil::KEY_LEFT) --x;
+				else if (k == rlutil::KEY_RIGHT) ++x;
+				else if (k == rlutil::KEY_UP) --y;
+				else if (k == rlutil::KEY_DOWN) ++y;
+				else if (k == rlutil::KEY_ESCAPE) break;
+				rlutil::locate(x,y); std::cout << "@" << std::endl; // Output player
+			}
+		}
+	}
+
+	std::cout << "Test 9: Delay" << std::endl;
 	std::cout << "Next numbers should appear rapidly after each other (but not instantly)." << std::endl;
 	rlutil::msleep(500);
 	for (float t = 0; t <= 4; t += 0.33333333f) {
