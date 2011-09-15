425c944d90d44be8b54ad345bf34cd16255cebf3
Tapio Vierros <tapiovierros@gmail.com>
2011-09-15 17:09:19 +0300

    Added cross-compilation script for easy Win32 testing from Linux.

---
+++ cross-compile.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+# Script name: build-mingw.sh
+# Description: builds examples with migw32 cross-compiler
+
+FLAGS="-Wall"
+CROSSPREFIX="i686-pc-mingw32-"
+
+${CROSSPREFIX}g++ $FLAGS test.cpp -o test.exe || exit 1
+
+${CROSSPREFIX}g++ $FLAGS example.cpp -o example-cpp.exe || exit 1
+
+${CROSSPREFIX}gcc $FLAGS example.c -o example-c.exe || exit 1
+
